85fc49bebfdaa4531cc89ca7328e68c2ded42264
Davidobot <d77777k@gmail.com>
2020-07-05 17:07:22 +0100

    Clean up build file

--- build_lovejs.sh
+++ build_lovejs.sh
@@ -6,20 +6,20 @@ mkdir -p build/debug build/release
 
 source ~/git/emsdk/emsdk_env.sh
 
-#(
-  # cd build/debug
-  # emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Debug
-  # emmake make -j 6
-  # cp love/love.js* ../../src/debug
-  # cp love/pthread-main.js ../../src/debug
-#)
+(
+  cd build/debug
+  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Debug
+  emmake make -j 6
+  cp love/love.js* ../../src/debug
+  cp love/love.wasm ../../src/debug
+  cp love/love.worker.js ../../src/debug
+)
 
 (
   cd build/release
-  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON
+  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
   emmake make -j 6
   cp love/love.js* ../../src/release
   cp love/love.wasm ../../src/release
   cp love/love.worker.js ../../src/release
-  #cp love/pthread-main.js ../../src/release
 )
