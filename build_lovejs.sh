e369e41406e7d7591574f7e9e676d7562fee4b4e
David Khachaturov <d77777k@gmail.com>
2020-09-12 10:17:49 +0100

    - Clean up build_lovejs.sh

--- build_lovejs.sh
+++ build_lovejs.sh
@@ -2,21 +2,12 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-mkdir -p build/debug build/release
+mkdir build
 
 source ~/git/emsdk/emsdk_env.sh
 
-# (
-#   cd build/debug
-#   emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Debug
-#   emmake make -j 6
-#   cp love/love.js* ../../src/debug
-#   cp love/love.wasm ../../src/debug
-#   cp love/love.worker.js ../../src/debug
-# )
-
 (
-  cd build/release
+  cd build
   emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
   emmake make -j 6
   cp love/love.js* ../../src/release
