8d1061c1a061ea37ebc9ab11005ba943f64218b9
Alexander Griffith <griffitaj@gmail.com>
2022-03-16 18:51:27 -0400

    Love 11.4 running

--- build_lovejs.sh
+++ build_lovejs.sh
@@ -4,11 +4,11 @@ cd "$(dirname "$0")"
 
 mkdir -p build/release build/compat
 
-source ~/git/emsdk/emsdk_env.sh
+source ~/Github/emsdk/emsdk_env.sh
 
 (
   cd build/release
-  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
+  emcmake cmake /home/alexjgriffith/Github/love.js/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
   emmake make -j 6
   cp love/love.js* ../../src/release
   cp love/love.wasm ../../src/release
@@ -17,7 +17,7 @@ source ~/git/emsdk/emsdk_env.sh
 
 (
   cd build/compat
-  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1
+  emcmake cmake /home/alexjgriffith/Github/love.js/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0
   emmake make -j 6
   cp love/love.js* ../../src/compat
   cp love/love.wasm ../../src/compat
